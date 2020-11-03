be2815ea95df4613ae206c8d1b7dcaf207a80bc1
Cordelia Wang <cordelia@Cordelias-MacBook-Pro.local>
2020-11-03 23:34:40 +0000

    Moved the plot function to the Graph_Plot file

--- Base.py
+++ Base.py
@@ -14,6 +14,7 @@ Assumptions:
     5. No electric and magnetic field fluctuations
     6. Ignoring spontaneous decay for Rabi Oscillation
 """
+
 def ExciteOrNot(excit_prob):
     '''
     under a given excitation probability, does a single ion get excited or not?
@@ -24,9 +25,11 @@ def ExciteOrNot(excit_prob):
     else:
         EoN = False
     return EoN
+
 def RabiOsci(t, Omega, Omega_0):
     '''
-    Excitation probability after addressing the ion with a detuned radiation with time t under radi freq omega
+    Excitation probability after addressing the ion with a detuned radiation 
+    with time t under radi freq omega
     
     t: time duration of laser dressing (unit: s)
     Omega: Rabi oscillation (angular) frequency (unit: rad/s)
@@ -36,6 +39,11 @@ def RabiOsci(t, Omega, Omega_0):
     W = Omega / 2
 #    print(2*W*t)
     return A/2 * (1 - sp.cos(2 * W * t))
+
+def LambDicke(lamb, w):
+    k_wave = 2 * sp.pi / lamb
+    return sp.sqrt(cs.hbar * k_wave**2 / (2 * m * w))
+
 def EffRabiFreq(n, m, lamb, w, om_0):
     '''
     Calculate effective Rabi Freq for a transition
@@ -45,65 +53,46 @@ def EffRabiFreq(n, m, lamb, w, om_0):
     w: trap frequency
     om_0: original Rabi frequency
     '''
-    eta, n_diff = LambDicke(lamb, w), abs(n - m)
+    eta, n_diff = LambDicke(lamb, w), abs(n - m) 
+    # eta is the LD parameter for axial motion
     factor1 = sp.exp(- eta**2 / 2) * (eta**n_diff)
     factor2 = sp.sqrt(FactorialDiv(sp.amin([n, m]), sp.amax([n, m])))
-    factor3 = genlaguerre(sp.amin([n, m]), n_diff)(eta**2)
+    factor3 = genlaguerre(np.amin([n, m]), n_diff)(eta**2)
     return om_0 * factor1 * factor2 * abs(factor3)
-def LambDicke(lamb, w):
-    k_wave = 2 * sp.pi / lamb
-    return sp.sqrt(cs.hbar * k_wave**2 / (2 * m * w))
+
 def Boltzmann_state(T, w, size = 100):
     '''
     Generate Boltzmann distributed states
+    Typical initial temperature ~ 1mK, initial average quantum number of 
+    vibrational mode ~ 20-50
     
     w: trap frequency
     '''
     bolt_fac = cs.hbar * w / (cs.k * T)
     return sts.planck.rvs(bolt_fac, size = size)
+
 def Freq2Wav(lamb, dw):
     '''
     Return shifted wavelength (+ve dw for blue shift, -ve dw for red shift)
     '''
     w0 = cs.c / lamb * 2 * sp.pi
     return cs.c / (w0 + dw) * 2 * sp.pi
-def PlotRedSb(n0):
-    '''
-    Plot the relative strengths of the carrier, first and second sidebands at and below quantum number n0
-    '''
-    fig, ax = plt.subplots()
-    Om_carrier, Om_red, Om_blue = [], [], []
-    for i in range(2, n0):
-        Om_carrier.append(EffRabiFreq(i, i, L, wz, 1))
-        if i > 0:
-            Om_red.append(EffRabiFreq(i, i - 1, Freq2Wav(L, - wz), wz, 1))
-        if i > 1:
-            Om_blue.append(EffRabiFreq(i, i - 2, Freq2Wav(L, - 2 * wz), wz, 1))
-    ax.plot(Om_carrier, label = 'carrier')
-    ax.plot(Om_red, label = 'red1')
-    ax.plot(Om_blue, label = 'red2')
-    suum = sp.array(Om_carrier)**2 + sp.array(Om_red)**2 + sp.array(Om_blue)**2
-    ax.plot(suum, label = 'sum')
-    ax.legend()
-    return ax
-#PlotRedSb(60)
-n0, N = 50, 1000 # starts at n = 50 and does 100 times
+
+
+n0, N = 50, 100 # starts at n = 50 and does 100 times
 t_pulse = 1.5e-6 * 31 # duration of a single pulse
 n, alln = n0, [n0]
 for i in range(N):
-    print('How far:', i)
+    # print('How far:', i)
     Om_red = EffRabiFreq(n, n-1, Freq2Wav(L, - wz), wz, rb)
-#    print('Effective Rabi:', Om_red, 'Real Rabi:', rb)
+    # print('Effective Rabi:', Om_red, 'Real Rabi:', rb)
     Red_prob = RabiOsci(t_pulse, Om_red, rb)
-#    print('Excitation Prob:', Red_prob)
+    # print('Excitation Prob:', Red_prob)
     eon = ExciteOrNot(Red_prob)
     if eon:
         n = n - 1
-#    print('now:', n)
+    # print('now:', n)
     alln.append(n)
+print(alln)
     
     
-#%%
-
-# version control testing
-a = 3
